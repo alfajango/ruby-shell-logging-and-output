e1bfab711fce70af0510615ef492ed9b58743a28
Steve Schwartz <steve@alfajango.com>
2016-04-28 17:48:09 -0400

    Added comments to ruby runner as to what each method succeeds/fails at.

--- ruby_err_test.rb
+++ ruby_err_test.rb
@@ -13,32 +13,71 @@ class RubyErrTest
 
   def run_err(input:"success", with:"bash")
     puts "Ruby running input \"#{input}\" with #{with}"
+
+    # Desired output would be:
+      # stdout to Ruby: yes
+      # stderr to Ruby: yes
+      # result to Ruby: yes
+      # stdout to logs: yes
+      # stderr to logs: yes
     case with
     when "bash"
+      # stdout to Ruby: yes
+      # stderr to Ruby: no
+      # result to Ruby: yes
+      # stdout to logs: no
+      # stderr to logs: no
       output = bash("bash #{script} #{input}")
       result = $?
       error = nil
     when "sh"
+      # stdout to Ruby: yes
+      # stderr to Ruby: no
+      # result to Ruby: yes
+      # stdout to logs: no
+      # stderr to logs: no
       output = %x(bash #{script} #{input})
       result = $?
       error = nil
     when "sh_with_redirected_stderr"
+      # stdout to Ruby: yes
+      # stderr to Ruby: yes/no (yes but combined with stdout)
+      # result to Ruby: yes
+      # stdout to logs: no
+      # stderr to logs: no
       output = %x(bash #{script} #{input} 2>&1)
       result = $?
       error = nil
     when "sh_with_log"
+      # => syntax error due to bash process substitution being run with `sh`
       output = %x(bash #{script} #{input} > >(tee -a #{output_log}) 2> >(tee -a #{error_log} >&2))
       result = $?
       error = nil
     when "bash_with_log"
+      # stdout to Ruby: no
+      # stderr to Ruby: no
+      # result to Ruby: yes
+      # stdout to logs: yes
+      # stderr to logs: yes
       output = bash( "bash #{script} #{input} > >(tee -a #{output_log}) 2> >(tee -a #{error_log} >&2)" )
       result = $?
       error = nil
     when "capture3"
+      # stdout to Ruby: yes
+      # stderr to Ruby: yes
+      # result to Ruby: yes
+      # stdout to logs: no
+      # stderr to logs: no
       output, error, result = Open3.capture3("bash", script, input)
     when "capture3_with_log_as_one_command"
+      # => syntax error due to bash process substitution being run with `sh`
       output, error, result = Open3.capture3("bash #{script} #{input} > >(tee -a #{output_log}) 2> >(tee -a #{error_log} >&2)")
     when "capture3_with_log_as_arguments"
+      # stdout to Ruby: yes
+      # stderr to Ruby: yes
+      # result to Ruby: yes
+      # stdout to logs: no
+      # stderr to logs: no
       output, error, result = Open3.capture3("bash", script, input, "> >(tee -a #{output_log}) 2> >(tee -a #{error_log} >&2)")
     end
     puts "Ruby result: #{result.inspect}"

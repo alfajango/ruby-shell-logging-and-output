f9ef235f029afb1bf8ac006904f8c9dcc09031fd
Steve Schwartz <steve@alfajango.com>
2016-04-28 18:03:45 -0400

    Updated script output to more clearly delineate between script output and ruby output.

--- err_test.sh
+++ err_test.sh
@@ -1,10 +1,10 @@
 SUCCESS=${1:-none}
 
-echo "This is running with input: \"$SUCCESS\"..."
+echo "Script stdout: Running with input \"$SUCCESS\"..."
 
 if [ "$SUCCESS" = "success" ]; then
-  echo "Success!"
+  echo "Script stdout: Success!"
 else
-  >&2 echo "Fail!"
+  >&2 echo "Script stderr: Fail!"
   exit 1
 fi
